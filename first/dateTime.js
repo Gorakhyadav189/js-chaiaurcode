9766f811a98316d1a809d585d4e734aa822db833
Gorakhyadav189 <gorakhyadav189@gmail.com>
2025-04-11 04:22:15 +0000

    arrayes

---
+++ first/dateTime.js
@@ -0,0 +1,8 @@
+let mydate=new Date();
+// console.log(mydate.toDateString());
+//Thu Apr 10 2025
+// console.log(mydate.toString());
+//Thu Apr 10 2025 05:40:14 GMT+0000 (Coordinated Universal Time)
+console.log(mydate.toLocaleDateString());
+//4/10/2025
+console.log(typeof mydate)
---
+++ second/arrays.js
@@ -0,0 +1,13 @@
+const array=[1,2,3,4,5,6,7];
+
+array.push(10);
+// array.pop()->is alwayes pop the last vindex value
+// array.push()->alwayes insert the value in last index
+// array.unshift()insert the value oi 0 index
+// array.shift()drop the value of first index
+// (array.includes(2)->to check the numer is in my array or not
+// console.log(array.includes(2))
+// .join()->is used to convert array to String
+// const newarray=array.join();
+// console.log(array);
+// console.log(newarray);
