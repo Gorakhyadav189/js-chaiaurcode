b4421a6324ff07bfd0494bd84f560725b54a79da
Gorakhyadav189 <gorakhyadav189@gmail.com>
2025-04-11 06:23:27 +0000

    commi

---
+++ second/function.js
@@ -0,0 +1,30 @@
+function addTwonum(num1,num2){
+
+    return num1+num2;
+}
+addTwonum(2,3);
+ const num=addTwonum(4,4);
+//  console.log(num);
+
+ function userdetails(username){
+    return `${username} this is user name`;
+ }
+ const name=userdetails("gorakh");
+//  console.log(name);
+
+ function calculatecartprice(...num1){         // we can get multiple vale and retur these bvalu inro array
+    return num1;
+ }
+//  console.log(calculatecartprice(23,24,23,25));
+
+ const obj={
+    name:"gorakh",
+    email:"yadav@gmail.com"
+ }
+  function forobject(anyobject){
+    console.log(`name is ${anyobject.name} and email is${anyobject.email}`);
+
+  }
+  forobject(obj);
+
+  ///-------------------------------------------------------scope-------------//
---
+++ second/object.js
@@ -0,0 +1,14 @@
+const user={
+    name:"gorakh",
+    age:24,
+    email:"yafav@gamil.com",
+    login_day:["monday","saturday"]
+}
+console.log(user.name);
+
+
+// const user={}//non single term objects
+// const user =new Object(); single term objectsa
+// Object.assign is used to join two boject
+// we can also combine two array or object using let user3={...user1,...user2}
+// learn destructuring of object
